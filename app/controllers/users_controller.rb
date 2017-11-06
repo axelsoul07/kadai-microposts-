4202a375c370522300e04846c7d207237eb36b71
axel <saitou9876sai@yahoo.co.jp>
2017-11-06 15:04:17 +0000

    kadai favorite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :favoriteings]
   
   def index
     @users = User.all.page(params[:page])
@@ -36,6 +36,13 @@ class UsersController < ApplicationController
   def followers
     @user = User.find(params[:id])
     @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+  
+  def favoriteings
+    @user = User.find(params[:id])
+    @favoriteings = @user.favoriteings.order('created_at DESC').page(params[:page])
+
     counts(@user)
   end
   
